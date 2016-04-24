fcd7acb09b5e740fa1ca03947b37ecb55b77a129
aorin <aorin@users.noreply.github.com>
2016-04-24 18:47:50 +0300

    Tennis refaktoroitu

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,9 +1,9 @@
 package ohtu;
 
 public class TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -13,68 +13,65 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals(player1Name)) {
+            player1Score++;
+        } else {
+            player2Score++;
+        }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        StringBuilder sb = new StringBuilder();
+
+        if (player1Score == player2Score) {
+            appendScoreWhenEven(sb);
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            appendScoreWhenAdvantageOrWin(sb);
+        } else {
+            appendScoreInOtherSituations(sb);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        return sb.toString();
+    }
+
+    private void appendScoreWhenEven(StringBuilder sb) {
+        if (player1Score == 4) {
+            sb.append("Deuce");
+        } else {
+            sb.append(scoreToString(player1Score));
+            sb.append("-All");
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+    }
+
+    private void appendScoreWhenAdvantageOrWin(StringBuilder sb) {
+        int score1MinusScore2 = player1Score - player2Score;
+        if (score1MinusScore2 == 1) {
+            sb.append("Advantage player1");
+        } else if (score1MinusScore2 == -1) {
+            sb.append("Advantage player2");
+        } else if (score1MinusScore2 >= 2) {
+            sb.append("Win for player1");
+        } else {
+            sb.append("Win for player2");
         }
-        return score;
+    }
+    
+    private void appendScoreInOtherSituations(StringBuilder sb) {
+        sb.append(scoreToString(player1Score));
+        sb.append("-");
+        sb.append(scoreToString(player2Score));
+    }
+
+    private String scoreToString(int score) {
+        switch (score) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+        }
+        return "";
     }
 }
